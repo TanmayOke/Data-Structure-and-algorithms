3b315969607e377a1c46a10be5e38a31fe0bdb5f
TanmayOke <oketanmay@gmail.com>
2026-07-30 21:27:58 +0530

    Firstcommit

---
+++ 01Basics/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << " apna college1\n";
+    cout << " apna college2\n";
+    cout << " apna college3\n";
+    cout << " apna college4";
+}
