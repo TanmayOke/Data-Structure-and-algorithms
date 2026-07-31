b0b948e8a74b6fd4da32182a91df694acc2b5176
TanmayOke <oketanmay@gmail.com>
2026-07-31 22:06:53 +0530

    Conditionals done

---
+++ 01Basics/cond.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // int age;
+    // cout<< "Enter your age";
+    // cin>>age;
+
+    // if(age>=18){
+    //     cout<<"You can vote";
+    // }else{
+    //     cout<<"you cant vote";
+    // }
+        
+    
+    
+        //ASCII VALUES
+        //A-Z ==>[65-90]
+        //a-z==>[97---]
+
+// char ch;
+//     cout<<"Enter the character:-";
+//     cin>>ch;
+
+    // if(ch>='a' && ch<='z'){
+    //     cout<< "LOWERCASE\n";
+    // }else{
+    //     cout<<"UPPERCASE\n";
+    // }
+
+    //    if(ch>=65 && ch<=90){
+    //     cout<< "UPPERCASE\n";
+    // }else{
+    //     cout<<"LOWERCASE\n";
+    // }
+
+cout<<"Hello world";
+    // int marks;
+    // cout<< "Enter your marks:- ";
+    // cin>>marks;
+
+    // if(marks>=90){
+    //     cout<<"A";
+    // }else if (marks>=80 && marks<90)
+    // {
+    //    cout<<"B";
+    // }
+    // else if (marks>=60 && marks<80)
+    // {
+    //      cout<<"C";
+    // }
+    // else if(marks<60)
+    // {
+    //     cout<<"D";
+    // }else{
+    //     cout<<"F";
+    // }
+    
+
+    int n ;
+    cout<<"Enter the number";
+    cin>>n;
+
+    cout<< ((n>0)?"Positive":"Negative");
+
+    return 0;
+}
