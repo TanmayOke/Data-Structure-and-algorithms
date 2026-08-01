3e34c99b716ad19771c43ba362cd747bc2a7df84
TanmayOke <oketanmay@gmail.com>
2026-08-01 09:55:55 +0530

    patterns2

--- 01Basics/patterns.cpp
+++ 01Basics/patterns.cpp
@@ -67,5 +67,55 @@ int main(){
         }
         cout << endl;
     }
+
+    //=======  PROBLEM 6 ================//
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i+1;j++){
+            cout<< "* ";
+        }
+        cout<< endl;
+    }
+
+    //=======  PROBLEM 7 ================//
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i+1;j++){
+            cout<<i+1<<" ";
+        }
+        cout<< endl;
+    }
+
+//=======  PROBLEM 8 ================//
+    char character='A';
+     for(int i=0;i<n;i++){
+    
+        for(int j=0;j<i+1;j++){
+            cout<<character<<" ";
+           
+        }
+          character++;
+        cout<< endl;
+    }
+
+//====Problem 9 ===========/
+// for(int i=0;i<n;i++){
+//     int num=1;
+//     for(int j=0;j<i;j++){
+//         cout<<num<<" ";
+//         num++;
+//     }
+//     cout<<endl;
+// }
+
+for(int i=0;i<n;i++){
+    
+    for(int j=1;j<=i+1;j++){
+        cout<<j<<" ";
+      
+    }
+    cout<<endl;
+}
+
 return 0;
 }
