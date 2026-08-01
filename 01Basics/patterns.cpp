b354e3fbc1453e225292eb61e202181732ff8157
TanmayOke <oketanmay@gmail.com>
2026-08-01 09:26:34 +0530

    pattern

---
+++ 01Basics/patterns.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<< "Enter the Number:-";
+    cin>>n;
+
+
+       // problem 1
+     cout<<"Number \n";
+     cout<<(" \n");
+    for(int i =1;i<=n;i++){
+        for(int i=1;i<=n;i++){
+            cout<< i<< " ";
+        }
+        cout<< endl;
+    }
+
+
+       // problem 2
+    cout<<"Star \n";
+    cout<<(" \n");
+
+      for(int i =1;i<=n;i++){
+        for(int i=1;i<=n;i++){
+            cout<< "*"<< " ";
+        }
+        cout<< endl;
+    }
+
+
+       // problem 3
+    cout<<"Alphabet \n";
+    cout<<(" \n");
+      for(int i =1;i<=n;i++){
+        char ch='A';
+        for(int i=1;i<=n;i++){
+            cout<< ch<< " ";
+            ch=ch+1;
+        }
+        cout<< endl;
+    }
+
+
+       // problem 4
+    int num=1;
+
+    for(int i =0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<< num << " ";
+            num++;
+        }
+        cout << endl;
+    }
+
+
+
+
+    // problem 5
+    char ch='A';
+
+    for(int i =0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<< ch << " ";
+            ch++;
+        }
+        cout << endl;
+    }
+return 0;
+}
