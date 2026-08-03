a1aff8a913d958821a545721680d531361562a16
TanmayOke <oketanmay@gmail.com>
2026-08-03 09:45:12 +0530

    Againcommit

--- 01Basics/functions.cpp
+++ 01Basics/functions.cpp
@@ -31,10 +31,26 @@ int factorial(int n){
     }
     return fact;
 }
+
+void changeValue(int a){
+    a*=2;
+    cout<<"Value of a inside function: "<<a<<endl;
+}
 int main(){
     hello();
     int summation=sum(556,798789);
     cout<<"Summation is: "<<summation<<endl;
+
+    //PASS BY VALUE!
+    int x=66,y=88;
+    cout<<"Summation is: "<<sum(x,y)<<endl;
+
+    int a=15;
+    cout<<"Value of a before function call: "<<a<<endl;
+    changeValue(a);
+    cout<<"Value of a after function call: "<<a<<endl;
+    //========COPY OF VARIABLES IS MADE AND PASSED TO FUNCTION========//
+
     cout<<"Minimum is: "<<min(444,7)<<endl;
     cout<<"Sum of first 10 numbers is: "<<sumOfNnumbers(10)<<endl;
     cout<<"Factorial of 5 is: "<<factorial(5)<<endl;
