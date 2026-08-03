087117d6da55f7a623859fd5c89b86ff63b8555e
TanmayOke <oketanmay@gmail.com>
2026-08-03 16:59:05 +0530

    BitWiseOperator

---
+++ 01Basics/BitwiseOperator.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;    
+int main(){
+int a=4,b=8;
+cout<<"Bitwise AND of "<<a<<" and "<<b<<" is "<<(a&b)<<endl;
+cout<<"Bitwise OR of "<<a<<" and "<<b<<" is "<<(a|b)<<endl;     
+cout<<"Bitwise XOR of "<<a<<" and "<<b<<" is "<<(a^b)<<endl;
+
+    return 0;
+}
