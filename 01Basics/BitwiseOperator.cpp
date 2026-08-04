5759f267d05d8e5eff052c2295c7f3520e6c18cd
Tanmay Oke <oketanmay@gmail.com>
2026-08-04 19:36:36 +0530

    Duplicate Bitwise XOR output for clarity
    Added duplicate output for Bitwise XOR operation.

--- 01Basics/BitwiseOperator.cpp
+++ 01Basics/BitwiseOperator.cpp
@@ -5,6 +5,7 @@ int a=4,b=8;
 cout<<"Bitwise AND of "<<a<<" and "<<b<<" is "<<(a&b)<<endl;
 cout<<"Bitwise OR of "<<a<<" and "<<b<<" is "<<(a|b)<<endl;     
 cout<<"Bitwise XOR of "<<a<<" and "<<b<<" is "<<(a^b)<<endl;
+cout<<"Bitwise XOR of "<<a<<" and "<<b<<" is "<<(a^b)<<endl;
 
     return 0;
 }
