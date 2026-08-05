b1fba2c66196b8e225b59191c72747c241be67da
TanmayOke <oketanmay@gmail.com>
2026-08-05 10:19:09 +0530

    fgdgd

--- 02Arrays/LinearSearch.cpp
+++ 02Arrays/LinearSearch.cpp
@@ -11,10 +11,24 @@ int linearSearch(int arr[],int sz,int target){
     }
     return -1;
 }
+
+void reverseArray(int arr[],int size){
+    int start=0;
+    int end =size-1;
+    while(start<end){
+        swap(arr[start],arr[end]);
+        start++;
+        end--;
+    }
+}
 int main(){
     int arr[5]={22,55,44,66,88};
     int target=55;
     int sz=5;
     cout<<linearSearch(arr,sz,target)<<endl;
+    reverseArray(arr,5);
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<" ";
+    }
     return 0;
 }
