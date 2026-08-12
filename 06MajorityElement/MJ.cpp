31a618f25385a36f474f01a4176e10a22352b49a
TanmayOke <oketanmay@gmail.com>
2026-08-12 09:59:28 +0530

    Hogya

--- 06MajorityElement/MJ.cpp
+++ 06MajorityElement/MJ.cpp
@@ -39,3 +39,20 @@ int majorityElement(vector<int>& nums) {
        } 
      return ans;
     }
+
+
+    int majorityElement(vector<int>& nums) {
+       int freq=0,ans=0;
+       for(int i=0;i<nums.size();i++){
+        if(freq==0){
+            ans=nums[i];
+        }
+        if(ans==nums[i]){
+            freq++;
+        }else{
+            freq--;
+        }
+       
+       }
+       return ans;
+    }
